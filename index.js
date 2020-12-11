7c9fa69dbc6ad5e66303779a647f86be4591295a
jacobmbogo <73630264+jacobmbogo@users.noreply.github.com>
2020-12-11 11:25:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("Hello World!");
+let sumTotal = 0;
+function sum(array){
+  for(i = 0; i<array.length; i++){
+    sumTotal = sumTotal + array[i];
+  }
+}
+sum(array);
+console.log(sumTotal);
