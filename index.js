1425965fdb7c4b165249aa8ae75682db74a468f3
Jacob Mbogo <jacobmbogo20000@gmail.com>
2021-02-22 13:12:25 +0300

    added some experimental json files

--- index.js
+++ index.js
@@ -1407,3 +1407,37 @@ fifteen.then(answer => console.log(`We got ${answer}`));
 let abby = ["jane","pita", "cuba"];
 let regex = /cuba/gi;
 console.log(regex.exec(abby));
+
+
+const getTodos = (resource) => {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+
+    request.addEventListener("readystatechange", () => {
+      // console.log(request, request.readyState);
+      if(request.readyState === 4 && request.status === 200){
+        const data = JSON.parse(request.responseText);
+        resolve(data);
+      } else if( request.readyState === 4){
+        reject("error getting results");
+      }
+    })
+
+    request.open("GET", resource);
+    request.send();
+
+
+ })
+};
+
+getTodos("todos/luigi.json").then(data => {
+  console.log("promise resolved:" ,data);
+  return getTodos("todos/mario.json");
+}).then(data => {
+  console.log("promise 2 resolved:",data);
+  return getTodos("todos/ogot.json");
+}).then(data => {
+  console.log("promise 3 resolved:", data);
+}).catch(err => {
+  console.log("promise rejected:", err);
+})
